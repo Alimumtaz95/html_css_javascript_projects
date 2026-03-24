097ef6018f110b5260ee8ec3a99bd873ec3365ac
Mumtaz Ali <mumtazalipintto@gmail.com>
2026-03-24 02:38:06 -0700

    feat: implement basic to-do list functionality with add, delete, and local storage persistence.

--- 03_level-3-todo/script.js
+++ 03_level-3-todo/script.js
@@ -8,7 +8,14 @@ if (savedData !== null) {
 
 taskDB.forEach(function (item) {
     let restoredTask = document.createElement("li");
-    restoredTask.textContent = item;
+    restoredTask.textContent = item + " (Delete Task)";
+    restoredTask.addEventListener("click", function () {
+        this.remove();
+        let positionForEach = taskDB.indexOf(item);
+        taskDB.splice(positionForEach, 1);
+        let textVersion = JSON.stringify(taskDB);
+        localStorage.setItem("myLocalStorageDB", textVersion);
+    });
     let list = document.getElementById("task-list");
     list.appendChild(restoredTask);
 });
@@ -27,6 +34,10 @@ addBtn.addEventListener("click", function () {
     let newTaskBullet = document.getElementById("task-list");
     newTask.addEventListener("click", function () {
         this.remove();
+        let position = taskDB.indexOf(taskInput.value);
+        taskDB.splice(position, 1);
+        let textVersion = JSON.stringify(taskDB);
+        localStorage.setItem("myLocalStorageDB", textVersion);
     });
     newTaskBullet.appendChild(newTask);
     taskInput.value = "";
